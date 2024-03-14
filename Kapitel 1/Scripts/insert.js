4fcd095880eb772dc99a6944fd411e3f2342b156
Lorin Sinzig <132986698+lorinsinzig@users.noreply.github.com>
2024-03-14 08:45:08 +0100

    Update insert.js

--- Kapitel 1/Scripts/insert.js
+++ Kapitel 1/Scripts/insert.js
@@ -1,4 +1,6 @@
-// 1.6 - Einfügen von Daten
+/////////////////////
+// 1.6
+///////////////////
 
 // Populate Kunde collection
 db.kunden.insertMany([
@@ -12,7 +14,8 @@ db.kunden.insertMany([
     "strasse": "Musterstraße 123",
     "ort": "Musterstadt",
     "plz": "12345",
-    "land": "Deutschland"
+    "land": "Deutschland",
+    "telefon": "+1234567890" // Added telefon attribute
   },
   {
     "_id": ObjectId(),
@@ -24,9 +27,33 @@ db.kunden.insertMany([
     "strasse": "Hauptstraße 456",
     "ort": "Stadt",
     "plz": "54321",
+    "land": "Deutschland",
+    "bemerkung": "Mehrmals negative Erfahrungen" // Added bemerkung attribute
+  },
+  {
+    "_id": ObjectId(),
+    "name": "Müller",
+    "vorname": "Max",
+    "email": "max.mueller@example.com",
+    "geburtsdatum": ISODate("1978-09-20"),
+    "geschlecht": "männlich",
+    "strasse": "Am See 7",
+    "ort": "Dorf",
+    "plz": "98765",
     "land": "Deutschland"
   },
-  // Add more kunden data if needed
+  {
+    "_id": ObjectId(),
+    "name": "Schulz",
+    "vorname": "Anna",
+    "email": "anna.schulz@example.com",
+    "geburtsdatum": ISODate("1995-03-10"),
+    "geschlecht": "weiblich",
+    "strasse": "Buchenweg 15",
+    "ort": "Stadt",
+    "plz": "23456",
+    "land": "Deutschland"
+  }
 ]);
 
 // Populate Buch collection
@@ -41,7 +68,36 @@ db.buecher.insertMany([
     "seitenanzahl": 320,
     "veröffentlichkeitsdatum": ISODate("1925-04-10")
   },
-  // Add more buecher data if needed
+  {
+    "_id": ObjectId(),
+    "iban": "978-3-456-78910-1",
+    "titel": "Harry Potter und der Stein der Weisen",
+    "autor_id": ObjectId("65e03b5389a9a5a25f2ba28b"),
+    "genre_id": ObjectId("65e03f52d3923867b055a0a9"),
+    "sprache_id": ObjectId("65e03fef0e1130e4f52e737f"),
+    "seitenanzahl": 336,
+    "veröffentlichkeitsdatum": ISODate("1997-06-26")
+  },
+  {
+    "_id": ObjectId(),
+    "iban": "978-3-789-10111-2",
+    "titel": "The Catcher in the Rye",
+    "autor_id": ObjectId("65e03b5389a9a5a25f2ba28b"),
+    "genre_id": ObjectId("65e03f52d3923867b055a0a9"),
+    "sprache_id": ObjectId("65e03fef0e1130e4f52e737f"),
+    "seitenanzahl": 277,
+    "veröffentlichkeitsdatum": ISODate("1951-07-16")
+  },
+  {
+    "_id": ObjectId(),
+    "iban": "978-3-456-78910-3",
+    "titel": "To Kill a Mockingbird",
+    "autor_id": ObjectId(), // New author
+    "genre_id": ObjectId("65e03f52d3923867b055a0a9"),
+    "sprache_id": ObjectId("65e03fef0e1130e4f52e737f"),
+    "seitenanzahl": 324,
+    "veröffentlichkeitsdatum": ISODate("1960-07-11")
+  }
 ]);
 
 // Populate Autor collection
@@ -53,7 +109,13 @@ db.autoren.insertMany([
     "geburtsjahr": 1896,
     "land": "USA"
   },
-  // Add more autoren data if needed
+  {
+    "_id": ObjectId(),
+    "name": "Salinger",
+    "vorname": "J.D.",
+    "geburtsjahr": 1919,
+    "land": "USA"
+  }
 ]);
 
 // Populate Sprache collection
@@ -62,7 +124,10 @@ db.sprachen.insertMany([
     "_id": ObjectId("65e03fef0e1130e4f52e737f"),
     "sprache": "Deutsch"
   },
-  // Add more sprachen data if needed
+  {
+    "_id": ObjectId(),
+    "sprache": "Englisch"
+  }
 ]);
 
 // Populate Genre collection
@@ -71,7 +136,10 @@ db.genres.insertMany([
     "_id": ObjectId("65e03f52d3923867b055a0a9"),
     "name": "Fiction"
   },
-  // Add more genres data if needed
+  {
+    "_id": ObjectId(),
+    "name": "Non-Fiction"
+  }
 ]);
 
 // Populate Autor-Buch collection
@@ -81,7 +149,21 @@ db.autor_buch.insertMany([
     "autor_id": ObjectId("65e03b5389a9a5a25f2ba28b"),
     "buch_id": ObjectId("65e03b31eef4cf59ec2e7c29")
   },
-  // Add more autor_buch data if needed
+  {
+    "_id": ObjectId(),
+    "autor_id": ObjectId("65e03b5389a9a5a25f2ba28b"),
+    "buch_id": ObjectId() // Reference to Harry Potter
+  },
+  {
+    "_id": ObjectId(),
+    "autor_id": ObjectId("65e03b5389a9a5a25f2ba28b"), // Reference to Fitzgerald
+    "buch_id": ObjectId()
+  },
+  {
+    "_id": ObjectId(),
+    "autor_id": ObjectId(), // Reference to Salinger
+    "buch_id": ObjectId()
+  }
 ]);
 
 // Populate Kunde-Buch collection
@@ -91,6 +173,5 @@ db.kunde_buch.insertMany([
     "kunde_id": ObjectId("65e03b1a1aa6382bf1eed24e"),
     "buch_id": ObjectId("65e03b31eef4cf59ec2e7c29"),
     "ausleihedatum": ISODate("2024-02-29")
-  },
-  // Add more kunde_buch data if needed
+  }
 ]);
